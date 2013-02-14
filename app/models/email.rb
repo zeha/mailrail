9b68076ae75631f3f67805c4d10e9d0f9e511334
Christian Hofstaedtler <christian@hofstaedtler.name>
2013-02-14 11:40:00 +0100

    fixes

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,3 @@
 class Email < ActiveRecord::Base
-  attr_accessible :attachments, :bodytext
+  attr_accessible :attachments, :bodytext, :local_part
 end
--- db/migrate/20130214095212_create_emails.rb
+++ db/migrate/20130214095212_create_emails.rb
@@ -3,6 +3,7 @@ class CreateEmails < ActiveRecord::Migration
     create_table :emails do |t|
       t.string :bodytext
       t.string :attachments
+      t.string :local_part
 
       t.timestamps
     end
